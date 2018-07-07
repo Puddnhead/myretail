c83f893b41f5d010c3e6dba63b6300cc3e469381
Mike Wilklow <michaelwilklow@gmail.com>
2018-07-07 12:20:53 -0500

    RequestValues unit test

---
+++ src/test/java/com/myretail/server/RequestValuesTest.java
@@ -0,0 +1,32 @@
+package com.myretail.server;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for {@link RequestValues}
+ *
+ * Created by MVW on 7/7/2018.
+ */
+public class RequestValuesTest {
+
+    @Test
+    public void testRequestValues() throws Exception {
+        Thread worker1 = new Thread(() -> {
+            RequestValues.setIsMasterOfTheUniverse(true);
+            assertThat(RequestValues.isMasterOfTheUniverse(), is(true));
+        });
+
+        Thread worker2 = new Thread(() ->
+            assertThat(RequestValues.isMasterOfTheUniverse(), is(false))
+        );
+
+        worker1.start();
+        worker2.start();
+        worker1.join();
+        worker2.join();
+    }
+
+}
